71464955066808a2e3a7a8db729464f7806d528a
edo9300 <edoardo762@gmail.com>
2020-12-06 18:13:44 +0100

    2020 day 6

---
+++ 2020/day 6.lua
@@ -0,0 +1,43 @@
+local part1,part2=0,0
+
+local curanswer=nil
+local curfullanswer=nil
+
+function check()
+	if curanswer then
+		for _,_ in pairs(curfullanswer) do
+			part1=part1+1
+		end
+		for _,_ in pairs(curanswer) do
+			part2=part2+1
+		end
+	end
+	curfullanswer=nil
+	curanswer=nil
+end
+
+for line in io.open("in6.txt"):lines() do
+	if line=="" then
+		check()
+	else
+		if not curanswer then
+			curfullanswer={}
+			curanswer={}
+			for c in line:gmatch("(.)") do
+				curanswer[c]=true
+				curfullanswer[c]=true
+			end
+		else
+			local tmpanswers={}
+			for c in line:gmatch("(.)") do
+				tmpanswers[c]=curanswer[c]
+				curfullanswer[c]=true
+			end
+			curanswer=tmpanswers
+		end
+	end
+end
+check()
+
+print("Part 1: "..part1)
+print("Part 2: "..part2)
