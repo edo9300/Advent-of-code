761e47078bd780dc59b43d83f22fbdad60a69658
edo9300 <edoardo762@gmail.com>
2022-12-04 23:34:16 +0100

    2022 day 3 update
    Use a single sed call to get an input already parsed as integers separated by spaces and run as little code as possible in subshells

--- 2022/day 03.sh
+++ 2022/day 03.sh
@@ -1,119 +1,9 @@
 #!/bin/sh
 
-get_priority() {
-	letter=$1
-	if [ "$letter" = "a" ]; then
-		return 1
-	elif [ "$letter" = "b" ]; then
-		return 2
-	elif [ "$letter" = "c" ]; then
-		return 3
-	elif [ "$letter" = "d" ]; then
-		return 4
-	elif [ "$letter" = "e" ]; then
-		return 5
-	elif [ "$letter" = "f" ]; then
-		return 6
-	elif [ "$letter" = "g" ]; then
-		return 7
-	elif [ "$letter" = "h" ]; then
-		return 8
-	elif [ "$letter" = "i" ]; then
-		return 9
-	elif [ "$letter" = "j" ]; then
-		return 10
-	elif [ "$letter" = "k" ]; then
-		return 11
-	elif [ "$letter" = "l" ]; then
-		return 12
-	elif [ "$letter" = "m" ]; then
-		return 13
-	elif [ "$letter" = "n" ]; then
-		return 14
-	elif [ "$letter" = "o" ]; then
-		return 15
-	elif [ "$letter" = "p" ]; then
-		return 16
-	elif [ "$letter" = "q" ]; then
-		return 17
-	elif [ "$letter" = "r" ]; then
-		return 18
-	elif [ "$letter" = "s" ]; then
-		return 19
-	elif [ "$letter" = "t" ]; then
-		return 20
-	elif [ "$letter" = "u" ]; then
-		return 21
-	elif [ "$letter" = "v" ]; then
-		return 22
-	elif [ "$letter" = "w" ]; then
-		return 23
-	elif [ "$letter" = "x" ]; then
-		return 24
-	elif [ "$letter" = "y" ]; then
-		return 25
-	elif [ "$letter" = "z" ]; then
-		return 26
-	elif [ "$letter" = "A" ]; then
-		return 27
-	elif [ "$letter" = "B" ]; then
-		return 28
-	elif [ "$letter" = "C" ]; then
-		return 29
-	elif [ "$letter" = "D" ]; then
-		return 30
-	elif [ "$letter" = "E" ]; then
-		return 31
-	elif [ "$letter" = "F" ]; then
-		return 32
-	elif [ "$letter" = "G" ]; then
-		return 33
-	elif [ "$letter" = "H" ]; then
-		return 34
-	elif [ "$letter" = "I" ]; then
-		return 35
-	elif [ "$letter" = "J" ]; then
-		return 36
-	elif [ "$letter" = "K" ]; then
-		return 37
-	elif [ "$letter" = "L" ]; then
-		return 38
-	elif [ "$letter" = "M" ]; then
-		return 39
-	elif [ "$letter" = "N" ]; then
-		return 40
-	elif [ "$letter" = "O" ]; then
-		return 41
-	elif [ "$letter" = "P" ]; then
-		return 42
-	elif [ "$letter" = "Q" ]; then
-		return 43
-	elif [ "$letter" = "R" ]; then
-		return 44
-	elif [ "$letter" = "S" ]; then
-		return 45
-	elif [ "$letter" = "T" ]; then
-		return 46
-	elif [ "$letter" = "U" ]; then
-		return 47
-	elif [ "$letter" = "V" ]; then
-		return 48
-	elif [ "$letter" = "W" ]; then
-		return 49
-	elif [ "$letter" = "X" ]; then
-		return 50
-	elif [ "$letter" = "Y" ]; then
-		return 51
-	elif [ "$letter" = "Z" ]; then
-		return 52
-	fi
-}
-
 get_sum_of_priorities() {
 	total=0
 	for line in "$@"; do
-		get_priority "$line"
-		total=$((total + $?))
+		total=$((total + line))
 	done
 }
 
@@ -125,20 +15,20 @@ split_rucksacks() {
 	sack_size=$(($# / 2))
 	first_half=""
 	second_half=""
-	counter=0
+	sack_counter=0
 	for var in "$@"; do
-		if [ $counter -lt $sack_size ]; then
+		if [ $sack_counter -lt $sack_size ]; then
 			first_half="${first_half} ${var}"
 		else
 			second_half="${second_half} ${var}"
 		fi
-		counter=$((counter + 1))
+		sack_counter=$((sack_counter + 1))
 	done
 	file=/tmp/aoc_2022_day3_part1_tmp1
 	write_unique_sorted_to_file "$first_half"
 	file=/tmp/aoc_2022_day3_part1_tmp2
 	write_unique_sorted_to_file "$second_half"
-	comm -12 /tmp/aoc_2022_day3_part1_tmp1 /tmp/aoc_2022_day3_part1_tmp2
+	get_sum_of_priorities $(comm -12 /tmp/aoc_2022_day3_part1_tmp1 /tmp/aoc_2022_day3_part1_tmp2)
 }
 
 counter=0
@@ -153,13 +43,22 @@ check_badges_part_2() {
 }
 
 
-while read -r line || [ -n "$line" ]; do #Thing needed so that no newline is needed at the end of the input
-	word_as_individual_chars=$(echo "$line" | fold -w1)
-	get_sum_of_priorities $(split_rucksacks $word_as_individual_chars)
+while read -r word_as_individual_chars; do
+	split_rucksacks $word_as_individual_chars
 	part_1=$((part_1 + total))
 	check_badges_part_2 $word_as_individual_chars
 	part_2=$((part_2 + total))
-done < input3.txt
+done <<EOF
+$(sed -e 's/a/1 /g' -e 's/b/2 /g' -e 's/c/3 /g' -e 's/d/4 /g' -e 's/e/5 /g' -e 's/f/6 /g' \
+-e 's/g/7 /g' -e 's/h/8 /g' -e 's/i/9 /g' -e 's/j/10 /g' -e 's/k/11 /g' -e 's/l/12 /g' \
+-e 's/m/13 /g' -e 's/n/14 /g' -e 's/o/15 /g' -e 's/p/16 /g' -e 's/q/17 /g' -e 's/r/18 /g' \
+-e 's/s/19 /g' -e 's/t/20 /g' -e 's/u/21 /g' -e 's/v/22 /g' -e 's/w/23 /g' -e 's/x/24 /g' \
+-e 's/y/25 /g' -e 's/z/26 /g' -e 's/A/27 /g' -e 's/B/28 /g' -e 's/C/29 /g' -e 's/D/30 /g' \
+-e 's/E/31 /g' -e 's/F/32 /g' -e 's/G/33 /g' -e 's/H/34 /g' -e 's/I/35 /g' -e 's/J/36 /g' \
+-e 's/K/37 /g' -e 's/L/38 /g' -e 's/M/39 /g' -e 's/N/40 /g' -e 's/O/41 /g' -e 's/P/42 /g' \
+-e 's/Q/43 /g' -e 's/R/44 /g' -e 's/S/45 /g' -e 's/T/46 /g' -e 's/U/47 /g' -e 's/V/48 /g' \
+-e 's/W/49 /g' -e 's/X/50 /g' -e 's/Y/51 /g' -e 's/Z/52 /g' input3.txt)
+EOF
 echo Part 1: "$part_1"
 echo Part 2: "$part_2"
 rm -f /tmp/aoc_2022_day3_part1_tmp1 /tmp/aoc_2022_day3_part1_tmp2 /tmp/aoc_2022_day3_part2_tmp0 /tmp/aoc_2022_day3_part2_tmp1 /tmp/aoc_2022_day3_part2_tmp2
