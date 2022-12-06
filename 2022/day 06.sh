d479f85698bd16cd9a67b9dc2417e427968d0335
Edoardo Lolletti <edoardo762@gmail.com>
2022-12-06 12:37:19 +0100

    2022 day 06: use actual POSIX awk commands
    It looks like using an empty string as FS makes it break if run with --posix on a linux system, split the string as individual chars and use space as separator instead

--- 2022/day 06.sh
+++ 2022/day 06.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 printf "Part 1 "
-awk 'BEGIN {FS=""}{for(i=1; i<=NF-4; i++) { unq=0; delete seen; for (j=i+0; j<i+4; j++){ if (!seen[$j]++) unq++;} if(unq > 3) {print i+3; exit 123;}}}' input6.txt
+sed -e 's/./& /g' input6.txt | awk 'BEGIN {FS=" "}{for(i=1; i<=NF-4; i++) { unq=0; delete seen; for (j=i+0; j<i+4; j++){ if (!seen[$j]++) unq++;} if(unq > 3) {print i+3; exit 123;}}}'
 printf "Part 2 "
-awk 'BEGIN {FS=""}{for(i=1; i<=NF-14; i++) { unq=0; delete seen; for (j=i+0; j<i+14; j++){ if (!seen[$j]++) unq++;} if(unq > 13) {print i+13; exit 123;}}}' input6.txt
+sed -e 's/./& /g' input6.txt | awk 'BEGIN {FS=" "}{for(i=1; i<=NF-14; i++) { unq=0; delete seen; for (j=i+0; j<i+14; j++){ if (!seen[$j]++) unq++;} if(unq > 13) {print i+13; exit 123;}}}'
