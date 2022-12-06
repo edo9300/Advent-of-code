467cbab16271abc2c11d79af05a7bbb4a3cd17ec
Edoardo Lolletti <edoardo762@gmail.com>
2022-12-06 12:22:52 +0100

    2022 day 6 part 2
    Rewrite it to use only awk

--- 2022/day 06.sh
+++ 2022/day 06.sh
@@ -1,44 +1,6 @@
 #!/bin/sh
 
-matched_chars=""
-char_1=""
-char_2=""
-char_3=""
-char_4=""
-
-put_new_char() {
-	matched_chars="$matched_chars$char_1"
-	char_1=$char_2
-	char_2=$char_3
-	char_3=$char_4
-	char_4=$1
-}
-
-print_match() {
-	#-1 as echo also puts a newline
-	echo "start-of-packet marker detected after $(($(echo $matched_chars$char_1$char_2$char_3$char_4 | wc -m) - 1)) characters"
-}
-
-are_at_least_all_set() {
-	[ -n "$char_1" ] && [ -n "$char_2" ] && [ -n "$char_3" ] && [ -n "$char_4" ]
-}
-
-check_chars() {
-	are_at_least_all_set && [ "$char_1" != "$char_2" ] && [ "$char_1" != "$char_3" ] && [ "$char_1" != "$char_4" ] && [ "$char_2" != "$char_3" ] && [ "$char_2" != "$char_4" ] && [ "$char_3" != "$char_4" ]
-}
-
-while read -r chars; do
-	for char in $chars; do
-		put_new_char $char
-		# check_chars
-		if check_chars; then
-			break 2
-		fi
-	done
-	echo Not Found
-	exit 1
-done <<EOF
-$(sed -e 's/./& /g' input6.txt)
-EOF
-
-print_match
+printf "Part 1 "
+awk 'BEGIN {FS=""}{for(i=1; i<=NF-4; i++) { unq=0; delete seen; for (j=i+0; j<i+4; j++){ if (!seen[$j]++) unq++;} if(unq > 3) {print i+3; exit 123;}}}' input6.txt
+printf "Part 2 "
+awk 'BEGIN {FS=""}{for(i=1; i<=NF-14; i++) { unq=0; delete seen; for (j=i+0; j<i+14; j++){ if (!seen[$j]++) unq++;} if(unq > 13) {print i+13; exit 123;}}}' input6.txt
