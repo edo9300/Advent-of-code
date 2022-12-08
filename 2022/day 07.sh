13899793ab59317c6fd367acce7159f9cc9f8484
edo9300 <edoardo762@gmail.com>
2022-12-08 18:16:06 +0100

    2022 day 7 part 2
    Also rewrite entirely the file counting algorithms as the previous one was pretty wrong and worked by chance only

--- 2022/day 07.sh
+++ 2022/day 07.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#We do this so that "sort" will sort strings based on their ascii values
+export LC_ALL=C
+
 cur_path=""
 
 go_up_one_path() {
@@ -17,22 +20,17 @@ change_path_to() {
 	else
 		cur_path="${cur_path}${1}/"
 	fi
-	# echo "Changed path, now pointing to $cur_path"
 }
 
 parse_command() {
 	if [ $1 = "$" ]; then
 		if [ $2 = "cd" ]; then
-			# echo "Changing directory to: ${3}"
 			change_path_to $3
-		# else
-			# echo "Listing contents in current directory"
 		fi
-	elif [ $1 != "dir" ]; then
-		# echo "We got a directory entry with name ${2}"
-	# else
-		# echo "We got a file entry"
-		echo "$cur_path${2} ${1}" >> /tmp/aoc_2022_day_7_part_1_files
+	elif [ $1 = "dir" ]; then
+		echo "$cur_path${2}/ _ 0" >> /tmp/aoc_2022_day_7_part_1_files
+	else
+		echo "$cur_path ${2} ${1}" >> /tmp/aoc_2022_day_7_part_1_files
 	fi
 }
 
@@ -44,17 +42,20 @@ done < input7.txt
 
 sort -u -r -o /tmp/aoc_2022_day_7_part_1_files /tmp/aoc_2022_day_7_part_1_files
 
+get_base_path() {
+	base_path=$(dirname $1)/
+	if [ $base_path = "//" ]; then
+		base_path="/"
+	fi
+}
+
 cur_path=""
 total=0
 
 parse_file() {
 	full_path=$1
-	size=$2
-	base_path=$(dirname $1)/
-	# echo $base_path
-	if [ $base_path = "//" ]; then
-		base_path="/"
-	fi
+	size=$3
+	base_path=$1
 	if [ "$cur_path" != "$base_path" ]; then
 		if [ -n "$cur_path" ]; then
 			printf "%s %012d\n" $cur_path $total >> /tmp/aoc_2022_day_7_part_1_files
@@ -65,74 +66,67 @@ parse_file() {
 	total=$((total + size))
 }
 
-for i in $(seq 1 1); do
-	cat /tmp/aoc_2022_day_7_part_1_files > /tmp/cur_input
-	echo "" > /tmp/aoc_2022_day_7_part_1_files
-	while read -r line || [ -n "$line" ]; do #Thing needed so that no newline is needed at the end of the input
-		if [ -z "$line" ]; then
-			break
-		fi
-		parse_file $line
-	done < /tmp/cur_input
-	if [ -n "$cur_path" ]; then
-		printf "%s %012d\n" $cur_path $total >> /tmp/aoc_2022_day_7_part_1_files
+cat /tmp/aoc_2022_day_7_part_1_files > /tmp/cur_input
+echo "" > /tmp/aoc_2022_day_7_part_1_files
+while read -r line || [ -n "$line" ]; do #Thing needed so that no newline is needed at the end of the input
+	if [ -z "$line" ]; then
+		break
 	fi
-	sort -u -r -o /tmp/aoc_2022_day_7_part_1_files /tmp/aoc_2022_day_7_part_1_files
-	cat /tmp/aoc_2022_day_7_part_1_files >> /tmp/aoc_2022_day_7_part_1_files_totals
-done
-cur_path=""
+	parse_file $line
+done < /tmp/cur_input
+if [ -n "$cur_path" ]; then
+	printf "%s %012d\n" $cur_path $total >> /tmp/aoc_2022_day_7_part_1_files
+fi
+sort -u -r -o /tmp/aoc_2022_day_7_part_1_files /tmp/aoc_2022_day_7_part_1_files
+cat /tmp/aoc_2022_day_7_part_1_files >> /tmp/aoc_2022_day_7_part_1_files_totals
+
 total=0
 
-parse_file2() {
+parse_folder() {
 	full_path=$1
 	size=$2
-	base_path=$(dirname $1)/
-	if [ "$full_path" = "$cur_path" ]; then
-		# echo "We got a match"
-		base_path=$full_path
-	elif [ $base_path = "//" ]; then
-		base_path="/"
-		# echo "$full_path $size"
-		# echo "$full_path $size" >> /tmp/aoc_2022_day_7_part_1_files_totals
-		# return 0
-	fi
-	# echo "Parsing file full_path: $full_path, size: $size, base_path: $base_path, current total size: $(printf "%012d\n" $total)"
-	if [ "$cur_path" != "$base_path" ]; then
+	get_base_path $1
+	printf "%s %012d\n" $base_path $size >> /tmp/aoc_2022_day_7_part_1_files
+	if [ "$cur_path" != "$full_path" ]; then
 		if [ -n "$cur_path" ]; then
 			printf "%s %012d\n" $cur_path $total >> /tmp/aoc_2022_day_7_part_1_files
 			total=0
 		fi
-		cur_path=$base_path
+		cur_path=$full_path
 	fi
-	# if [ $full_path != $cur_path ]; then
-		total=$((total + size))
-	# fi
+	total=$((total + size))
 }
+longest_path=$(sed 's/[^/]*//g' /tmp/aoc_2022_day_7_part_1_files | awk '{print length}'|sort -nr|head -1)
 
-for i in $(seq 1 15); do
+while [ "$longest_path" -ne 0 ]; do
+	cur_path=""
+	total=0
 	cat /tmp/aoc_2022_day_7_part_1_files > /tmp/cur_input
 	echo "" > /tmp/aoc_2022_day_7_part_1_files
 	while read -r line || [ -n "$line" ]; do #Thing needed so that no newline is needed at the end of the input
 		if [ -z "$line" ]; then
 			break
 		fi
-		parse_file2 $line
+		parse_folder $line
 	done <<EOF
-$(sed -e 's/ 0*/ /' -e '/^$/d' /tmp/cur_input)
+$(sed -e 's/ 0*/ /' -e '/^$/d' /tmp/cur_input | awk -FN -vcount=$longest_path 'BEGIN { FS = "/" } ;NF==count+1')
 EOF
 	if [ -n "$cur_path" ]; then
 		printf "%s %012d\n" $cur_path $total >> /tmp/aoc_2022_day_7_part_1_files
 	fi
-	sort -u -r -o /tmp/aoc_2022_day_7_part_1_files /tmp/aoc_2022_day_7_part_1_files
+	awk -FN -vcount=$longest_path 'BEGIN { FS = "/" } ;NF<=count' /tmp/cur_input >> /tmp/aoc_2022_day_7_part_1_files
+	sort -r -o /tmp/aoc_2022_day_7_part_1_files /tmp/aoc_2022_day_7_part_1_files
 	cat /tmp/aoc_2022_day_7_part_1_files >> /tmp/aoc_2022_day_7_part_1_files_totals
+	longest_path=$((longest_path - 1))
 done
-# sort -u -r -o /tmp/aoc_2022_day_7_part_1_files_totals
+
 sort -u -r -o /tmp/aoc_2022_day_7_part_1_files_totals /tmp/aoc_2022_day_7_part_1_files_totals
-awk '!_[$1]++' /tmp/aoc_2022_day_7_part_1_files_totals > /tmp/aoc_2022_day_7_part_1_files_totals_uniq
+#This awk command takes the first occurrence of each path so that the most recent one only is taken and all the others are discarded
+awk '!_[$1]++' /tmp/aoc_2022_day_7_part_1_files_totals | sed '/^$/d' > /tmp/aoc_2022_day_7_part_1_files_totals_uniq
 
 total=0
 sum_folder() {
-	folder_path=$1
+	# folder_path=$1
 	folder_size=$2
 	total=$((total + folder_size))
 }
@@ -144,9 +138,11 @@ EOF
 
 echo "Part 1: $total"
 
+used_space=$(awk '{ print $2 }' /tmp/aoc_2022_day_7_part_1_files_totals_uniq | sort | tail -n1 | sed 's/^0*//')
+unused_space=$((70000000 - used_space))
+space_to_free=$((30000000 - unused_space))
+echo Used space is $used_space
+echo Unused space is $unused_space
+echo Space to free is $space_to_free
 
-
-
-
-
-
+printf "Part 2: %d\n" "$(awk -v space_to_free="$space_to_free" '$2 >= space_to_free { print $2 }' /tmp/aoc_2022_day_7_part_1_files_totals_uniq  | sort | head -n1 | sed 's/^0*//')"
