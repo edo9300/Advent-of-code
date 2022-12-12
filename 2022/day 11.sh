62e5e029eca0ab8f4a5526950c773789256af365
Edoardo Lolletti <edoardo762@gmail.com>
2022-12-12 23:33:10 +0100

    2022 day 11 part 2

--- 2022/day 11.sh
+++ 2022/day 11.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -f
+
 divisors=1
-
-
+total_monkeys=0
 
 generate_monkey() {
 	eval "monkey_${current_monkey}_operator=$current_operator"
@@ -11,8 +11,8 @@ generate_monkey() {
 	eval "monkey_${current_monkey}_true_branch=$true_monkey"
 	eval "monkey_${current_monkey}_false_branch=$false_monkey"
 	eval "monkey_${current_monkey}_items='$monkey_items'"
+	total_monkeys=$((total_monkeys + 1))
 }
-total_monkeys=0
 
 do_thing() {
 	if [ "$1" = "Monkey" ]; then
@@ -33,17 +33,15 @@ do_thing() {
 		else
 			false_monkey="$6"
 			generate_monkey
-			total_monkeys=$((total_monkeys + 1))
 		fi
 	fi
 }
 
-while read -r line || [ -n "$line" ]; do #Thing needed so that no newline is needed at the end of the input
+while read -r line; do #Thing needed so that no newline is needed at the end of the input
 	do_thing $line
 done <<EOF
 $(sed -e 's/.*new = old \(.*\)/\1/' -e 's/.*items//' -e 's/[:,]//g' input11.txt) 
 EOF
-echo "MCM: $divisors"
 
 get_operator() {
 	eval "operator"='$'"monkey_${1}_operator"
@@ -112,44 +110,61 @@ print_monkey_inspection_stats() {
 	done
 }
 
-round_counter=0
-while [ "$round_counter" -lt 20 ]; do
-	current_monkey=0
-	while [ "$current_monkey" -lt "$total_monkeys" ]; do
-		# echo monkey: $current_monkey
-		get_operator $current_monkey
-		get_monkey_divisible_by $current_monkey
-		get_monkey_true_branch $current_monkey
-		get_monkey_false_branch $current_monkey
-		get_monkey_items $current_monkey
-		get_and_clear_monkey_items $current_monkey
-		iterable_items=$items
-		get_inspected_items $current_monkey
-		for item in $iterable_items; do
-			used_operand=$operand
-			if [ "$operator" = '+' ]; then
-				new=$((item + operand))
-			else
-				if [ "$operand" = "old" ]; then
-					used_operand=$item
+normalize() {
+	if [ "$part" -eq 1 ]; then
+		new=$((new / 3))
+	elif [ "$new" -ge "$divisors" ]; then
+		new=$((new % divisors))
+	fi
+}
+
+run_game() {
+	part=$1
+	round_counter=0
+	if [ "$part" -eq 1 ]; then
+		max=20
+	else
+		max=10000
+	fi
+	while [ "$round_counter" -lt $max ]; do
+		current_monkey=0
+		while [ "$current_monkey" -lt "$total_monkeys" ]; do
+			get_operator $current_monkey
+			get_monkey_divisible_by $current_monkey
+			get_monkey_true_branch $current_monkey
+			get_monkey_false_branch $current_monkey
+			get_monkey_items $current_monkey
+			get_and_clear_monkey_items $current_monkey
+			iterable_items=$items
+			get_inspected_items $current_monkey
+			for item in $iterable_items; do
+				used_operand=$operand
+				if [ "$operator" = '+' ]; then
+					new=$((item + operand))
+				else
+					if [ "$operand" = "old" ]; then
+						used_operand=$item
+					fi
+					new=$((item * used_operand))
 				fi
-				new=$((item * used_operand))
-			fi
-			new=$((new / 3))
-			if [ $((new % divisible_by)) -eq 0 ]; then
-				push_item_to_monkey $true_branch $new
-			else
-				push_item_to_monkey $false_branch $new
-			fi
-			inspected_items=$((inspected_items + 1))
+				normalize
+				if [ $((new % divisible_by)) -eq 0 ]; then
+					push_item_to_monkey $true_branch $new
+				else
+					push_item_to_monkey $false_branch $new
+				fi
+				inspected_items=$((inspected_items + 1))
+			done
+			set_inspected_items $current_monkey $inspected_items
+			current_monkey=$((current_monkey + 1))
 		done
-		set_inspected_items $current_monkey $inspected_items
-		current_monkey=$((current_monkey + 1))
+		round_counter=$((round_counter + 1))
 	done
-	round_counter=$((round_counter + 1))
-done
-part1=1
-for number in $(print_monkey_inspection_stats | sort -n | tail -n2); do
-	part1=$((part1 * number))
-done
-echo Part 1: $part1
+	result=1
+	for number in $(print_monkey_inspection_stats | sort -n | tail -n2); do
+		result=$((result * number))
+	done
+	echo $result
+}
+echo "Part 1: $(run_game 1)"
+echo "Part 2: $(run_game 2)"
